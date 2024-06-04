1073c8f64b96ba56332c3558a79c7822ffaa649f
wownepal <phurba.sherpa@wowfinstacknepal.com>
2024-06-04 17:46:34 +0545

    feat: implement api for patch and delete

--- automation_controller/api/views.py
+++ automation_controller/api/views.py
@@ -22,10 +22,6 @@ def users(request):
         confirm_password = request_data.get('confirm_password')
         request_data['mac_addr'] = 'ff-ff-ff-ff-ff-ff'
         
-        print('\n\n\n')
-        print(request_data)
-        print('\n\n\n')
-        
         # Check if password and confirm password matches
         if password != confirm_password:
             return JsonResponse(data = {"message": "password and confirm password doesn't match"}, status=status.HTTP_400_BAD_REQUEST)
@@ -41,6 +37,7 @@ def users(request):
     else:
         return JsonResponse('Method is not supported', status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
+# func that handles by id
 @api_view(['GET', 'PATCH', 'DELETE'])
 def user(request, email):
     try:
@@ -50,14 +47,21 @@ def user(request, email):
     
     # GET by Id
     if request.method == 'GET':
-        user_json = UserSerializer(user, many=False)
-        return JsonResponse(data=user_json.data, safe=False, status=status.HTTP_200_OK)
+        serialized_user = UserSerializer(user, many=False)
+        return JsonResponse(data=serialized_user.data, safe=False, status=status.HTTP_200_OK)
     
     # PATCH
     elif request.method == 'PATCH':
-        user.orange_hrm_password = 'or-password'
-        user.orange_hrm_username = 'or-user'
-        user.save()
-        return JsonResponse(UserSerializer(data=user), status=status.HTTP_200_OK)
+        request_data = JSONParser().parse(request)
+        serialized_user = UserSerializer(user, request_data, partial=True)
+        
+        if serialized_user.is_valid():
+            serialized_user.save()
+            return JsonResponse(serialized_user.data, status=status.HTTP_200_OK)
+        return JsonResponse({"message":serialized_user.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        user.delete()
+        return JsonResponse({}, status=status.HTTP_204_NO_CONTENT)
         
         
