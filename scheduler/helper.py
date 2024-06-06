248d89c59505a5ada1ca9ffcb4aaad95f92a4d8f
wownepal <phurba.sherpa@wowfinstacknepal.com>
2024-06-06 22:35:04 +0545

    docs(helpers-index): add explanation comments on params and func
    add comments on:
    1) static variable
    2) function describing its purpose

--- scheduler/helper.py
+++ scheduler/helper.py
@@ -4,20 +4,17 @@ import re
 import subprocess
 from datetime import datetime, time
 
-OFFINE_MSG = 'Destination host unreachable.'
-ONLINE_ECHO_FAILED_MSG = 'Request timed out.'
-ONLINE_ECHO_SUCCESS_MSG = '0% loss'
-
-PUNCH_IN_TIME = time(11, 0, 0)
-PUNC_OUT_TIME = time(7, 0, 0)
-
-PUNCH_STATUS = {
+# Parameters
+OFFINE_MSG = 'Destination host unreachable'     # Echo message from router if destination ip is offline/invalid
+ONLINE_ECHO_FAILED_MSG = 'Request timed out.'   # Echo message from router if destination ip's firewall blocks echo
+ONLINE_ECHO_SUCCESS_MSG = '0% loss'             # Echo message from router 
+PUNCH_IN_TIME_MAX_TH= time(11, 0, 0)            # Max threashold time for punch in (11:00:00)
+PUNC_OUT_TIME_MIN_TH = time(6, 0, 0)            # Min. threshold time for punch out (6:00:00)
+MAC_ADDR_PATTERN = '([-0-9a-f]{17})'            # Mac address pattern in arp table
+OPERATION_TYPES = {
     'PUNCH_IN': 'PI',
     'PUNCH_OUT': 'PO'
 }
-
-### will fetch the physical address from arp table
-mac_addr_pattern = '([-0-9a-f]{17})'
 
 def get_mac_addr_list(ip_addr_list):
     ip_mac_table = {}
@@ -27,7 +24,7 @@ def get_mac_addr_list(ip_addr_list):
         arp_info = result.stdout.decode('utf8')
         
         # Extract mac
-        matched_mac_addr_list = re.findall(mac_addr_pattern, arp_info)
+        matched_mac_addr_list = re.findall(MAC_ADDR_PATTERN, arp_info)
         
         # if matched then will have matched list, if not then []
         if len(matched_mac_addr_list) != 0:
@@ -35,12 +32,24 @@ def get_mac_addr_list(ip_addr_list):
     return ip_mac_table
 
 def ping_device(ip_addr):
+    """
+    Function to ping the ip and return the echo message
+    """
+
     ping_command = 'ping -n 1 '+ ip_addr    
     result = subprocess.run(ping_command, stdout=subprocess.PIPE)
     output = result.stdout.decode('utf8')
     return output
     
 def get_online_offline_devices(ip_addr_list):
+
+    """
+    1. For each of the ip-address, ping the device.
+    2. Check the message echoed from ping.
+    3. Based on the message echoed, insert the pinged ip in offline bucket or online bucket
+    4. Return both bucket
+    """
+
     online_ip_addr_list = []
     offline_ip_addr_list = []
     
@@ -53,9 +62,15 @@ def get_online_offline_devices(ip_addr_list):
 
     return online_ip_addr_list, offline_ip_addr_list
 
-def is_in_or_out_time():
+def get_operation_type():
+    """
+    Return the action type: punch in/out.
+    If it is before 11:00:00 then PI (PUNCH IN)
+    If it is after 06:00:00 then PO (PUNCH OUT)
+    """
+
     current_time = datetime.now().time()
-    if current_time <= PUNCH_IN_TIME:
-        return PUNCH_STATUS.get('PUNCH_IN')
-    elif current_time < PUNC_OUT_TIME:
-        return PUNCH_STATUS.get('PUNCH_OUT')
+    if current_time <= PUNCH_IN_TIME_MAX_TH:
+        return OPERATION_TYPES.get('PUNCH_IN')
+    elif current_time > PUNC_OUT_TIME_MIN_TH:
+        return OPERATION_TYPES.get('PUNCH_OUT')
--- scheduler/index.py
+++ scheduler/index.py
@@ -4,28 +4,37 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import time
-from helper import get_online_offline_devices
+from helper import get_online_offline_devices, get_operation_type
 
-
-
-url = 'http://185.185.127.219:8080/login.jsp'
+# Params
+URL = 'http://185.185.127.219:8080/login.jsp'   # Url of the orange website
+USERNAME_INPUT_ID = 'login-form-username'       # Id of username input element
+PASSWORD_INPUT_ID = 'login-form-password'       # Id of password input element
+SUBMIT_BTN_ID = 'login-form-submit'             # Id of submit btn
 username='phurba.sherpa'
 password='Phurb@12'
 
-username_input_id = 'login-form-username'
-password_input_id = 'login-form-password'
-submit_btn_id = 'login-form-submit'
-
 def main():
+
+    online_ips, offline_ips = get_online_offline_devices(['192.168.1.68'])
+    operation_type = get_operation_type()
+
+    if operation_type == 'PI':
+        print('Punch in process started...')
+        print('Punching in for ips', online_ips)
+    elif operation_type == 'PO':
+        print('Punch out process started...')
+        print('Punching out for ips', offline_ips)
+
     service = Service(executable_path='chromedriver.exe')
     driver = webdriver.Chrome(service=service)
-    driver.get(url)
+    driver.get(URL)
     WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.ID, username_input_id))
+        EC.presence_of_element_located((By.ID, USERNAME_INPUT_ID))
     )
 
-    username_input_el = driver.find_element(by=By.ID, value=username_input_id)
-    password_input_el = driver.find_element(by=By.ID, value=password_input_id)
+    username_input_el = driver.find_element(by=By.ID, value=USERNAME_INPUT_ID)
+    password_input_el = driver.find_element(by=By.ID, value=PASSWORD_INPUT_ID)
     
     # clear
     username_input_el.clear()
@@ -36,7 +45,7 @@ def main():
     password_input_el.send_keys(password)
 
     # submit
-    driver.find_element(by=By.ID, value=submit_btn_id).click()
+    driver.find_element(by=By.ID, value=SUBMIT_BTN_ID).click()
     heading = driver.find_element(By.TAG_NAME, 'h1')
     title = heading.text
 
